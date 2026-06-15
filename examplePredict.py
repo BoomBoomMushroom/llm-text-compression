7131cc213b5ad23e32aaf70e27109d57c2ce3d45
BoomBoomMushroom <77977795+BoomBoomMushroom@users.noreply.github.com>
2026-06-15 17:08:43 -0400

    working, example is better than huffman

--- examplePredict.py
+++ examplePredict.py
@@ -13,6 +13,8 @@ import tensorflow as tf
 stringToEncode = "coda started making their obelisks and then theo made a lavacast and now those are the \"Rockets\" space X makes"
 #stringToEncode = input("Enter string to encode: ")
 
+stringToEncode = """I have the first regular season chess match tomorrow after school against Madeira, and I'm just so happy to be playing (on an important board), after all of the effort I put in and just barely missing the top boards Sophomore and Junior year after all the work I put in, I was genuinely quite disappointed last year, but I at least got to be a starting board in the end-of season tournament where we got first (Not the playoffs). But really I felt like there was definitely a chance that that would be the only like direct showing of my efforts to improve all this much and make the team. Wyoming just has really good chess players bro but like I did it this year and I'm going to try my best to lock tf in this year."""
+
 # Load Model
 from llm.NextLetterModel import NextLetterModel
 from llm.OneStep import OneStep
@@ -293,7 +295,7 @@ def decompressText(data: bytearray) -> str:
 compressed = compressText(stringToEncode, True)
 print("Compressed: ", compressed, "\n")
 
-printHistogram()
+#printHistogram()
 
 with open("out.txt", "wb") as f: f.write(compressed)
 
